4ae745a8882e58c9e4ef36d9880cf3a60445cf10
Rusheel Iyer <rusheel2911@gmail.com>
2020-08-13 10:35:56 +0200

    Updates

---
+++ precache-manifest.65e8c67c014d355e5c641488a0cd62eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab1312610cbfc71692564c19e5fb19e8",
+    "url": "/webchat/index.html"
+  },
+  {
+    "revision": "5bbab288b2a67bf48269",
+    "url": "/webchat/static/css/main.9c60b1d7.chunk.css"
+  },
+  {
+    "revision": "cbb715b0313da8665b8c",
+    "url": "/webchat/static/js/2.4c17a22c.chunk.js"
+  },
+  {
+    "revision": "04749fd5e55d7c0f35ea22ffa0f8294b",
+    "url": "/webchat/static/js/2.4c17a22c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bbab288b2a67bf48269",
+    "url": "/webchat/static/js/main.49da021c.chunk.js"
+  },
+  {
+    "revision": "eed273ed08f8ebec7c03",
+    "url": "/webchat/static/js/runtime-main.581dfd24.js"
+  }
+]);
